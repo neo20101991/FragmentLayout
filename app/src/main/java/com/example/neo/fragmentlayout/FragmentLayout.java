ec273b143dc12f4010673903ea32b28a66c75104
Vivian Noronha <neo20101991@gmail.com>
2014-12-29 21:36:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/neo/fragmentlayout/FragmentLayout.java
@@ -0,0 +1,16 @@
+package com.example.neo.fragmentlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class FragmentLayout extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+    }
+
+    }
+
